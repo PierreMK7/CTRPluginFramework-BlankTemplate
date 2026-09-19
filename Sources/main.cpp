2a1d5cdb4cb96acfd681640ff83b353074010d0b
PierreMK7 <mkpg.mk7@gmail.com>
2026-09-19 03:23:44 +0200

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,41 +1,44 @@
 #include <3ds.h>
-#include "../Includes/CTRPluginFramework.h"
 
-// Définition de votre code d'optimisation (Version Europe)
+// Définition manuelle des fonctions minimales pour CTRPF
+namespace CTRPluginFramework {
+    class MenuEntry;
+    class PluginMenu {
+    public:
+        PluginMenu(const char* title, int major, int minor, int revision);
+        void Append(MenuEntry* entry);
+        void Run(void);
+    };
+    class MenuEntry {
+    public:
+        MenuEntry(const char* name, void (*func)(MenuEntry*));
+        bool IsActivated(void);
+        void SetDescription(const char* desc);
+    };
+}
+
+using namespace CTRPluginFramework;
+
+// Votre code d'optimisation parfait
 #define ADRESSE_OPTIMISATION   0x00451C60 
 #define VALEUR_OPTIMISATION    0xEA000007
 
-// Cette fonction s'exécute en continu en tâche de fond pendant que vous jouez
 void executionPerformance(MenuEntry *entry) {
     if (entry->IsActivated()) {
-        // Si l'option est cochée dans votre menu, on force l'injection du code en RAM
         *(volatile u32*)(ADRESSE_OPTIMISATION) = VALEUR_OPTIMISATION;
     }
 }
 
-// Configuration de votre menu unique
 void InitMenu(PluginMenu *menu) {
-    // Création de la ligne cochable dans le menu
     MenuEntry *entry = new MenuEntry("Disable Right Eye Rendering", executionPerformance);
-    
-    // Description qui s'affiche en bas de l'écran de la console
     entry->SetDescription("Ameliore massivement la fluidite en jeu en coupant le rendu de l'oeil droit.");
-    
-    // Ajout de l'option au menu principal
     menu->Append(entry);
 }
 
 int main(void) {
-    // IMPORTANT : On initialise le menu avec les options strictes (1, 0, 0)
-    // Cela indique au système CTRPF de désactiver COMPLÈTEMENT le menu de triche ("Search Menu")
-    // et l'éditeur hexadécimal. L'utilisateur ne verra QUE votre option.
+    // Initialisation stricte (1, 0, 0) pour masquer le menu de triche externe
     PluginMenu *menu = new PluginMenu("MK7 Optimization Plugin", 1, 0, 0);
-    
-    // On injecte notre ligne cochable
     InitMenu(menu);
-    
-    // On lance l'exécution du menu
     menu->Run();
-    
     return 0;
 }
